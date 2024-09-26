0ad9f0dae125aef1931fbc7c0aa7dc2ea6f6394c
gabriel <gabrielkauan236@gmail.com>
2024-09-26 07:49:28 -0300

    O método hasOwnProperty() retorna um booleano indicando se o objeto possui a propriedade especificada como uma propriedade definida no próprio objeto em questão (ao contrário de uma propriedade herdada).

--- main.js
+++ main.js
@@ -15,3 +15,29 @@ const limparFormulario = () =>{
 const eNumero = (numero) => /^[0-9]+$/.test(numero);
 // Verifica o tamanho do CEP
 const cepValido = (cep) =>  cep.length == 8 && eNumero(cep);
+
+// Função para preencher campos relacionados ao CEP
+const preencherFormulario = (endereco) =>{
+    document.getElementById('logradouro').value = endereco.logradouro;
+    //Colocar o valor de logradouro da API dentro do campo logradouro do formulário
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('localidade').value = endereco.localidade;
+    document.getElementById('uf').value = endereco.uf;
+}
+
+// Função para consumo de API ViaCEP
+const pesquisarCep = async() => {
+    limparFormulario();
+    const url = `https://viacep.com.br/ws/${cep.value}/json/`;
+    if(cepValido(cep.value)){
+        const dados = await fetch(url);
+        const addres = await dados.json();
+
+        if(addres.hasOwnProperty('erro')){
+
+        }
+
+    }else{
+
+    }
+}
